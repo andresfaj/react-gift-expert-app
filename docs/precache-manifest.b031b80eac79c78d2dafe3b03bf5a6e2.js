fc5eb6b1574d8428fde528d77229aeb307fade83
Andrés Achury <achuryjaramillo@gmail.com>
2020-11-29 20:48:43 -0500

    Docs added

---
+++ docs/precache-manifest.b031b80eac79c78d2dafe3b03bf5a6e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566fb85b052004dfd2fb07229929a93d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "943b163ec661a31b0419",
+    "url": "/static/css/main.fd35a761.chunk.css"
+  },
+  {
+    "revision": "17511b471bfdde6e14c9",
+    "url": "/static/js/2.ee48284c.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.ee48284c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "943b163ec661a31b0419",
+    "url": "/static/js/main.903aa1d7.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
